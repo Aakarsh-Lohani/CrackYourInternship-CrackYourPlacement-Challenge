138ab369547bdba4349e1dcc3daaa1b493d52659
AAKARSH LOHANI <aakarshlohani01@gmail.com>
2024-08-16 23:25:24 +0530

    Time Complexity : O(n)

---
+++ Mathematical/03_62_Maximum_Product_of_Three_Numbers.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/maximum-product-of-three-numbers/description/
+# 628. Maximum Product of Three Numbers
+
+# Time Complexity : O(n)
+# Space Complexity : O(1)
+from typing import List
+
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        max1 = max2 = max3 = float('-inf')
+        min1 = min2 = float('inf')
+
+        for num in nums:
+            if num > max1:
+                max3 = max2
+                max2 = max1
+                max1 = num
+            elif num > max2:
+                max3 = max2
+                max2 = num
+            elif num > max3:
+                max3 = num
+
+            if num < min1:
+                min2 = min1
+                min1 = num
+            elif num < min2:
+                min2 = num
+
+        return max(max1 * max2 * max3, min1 * min2 * max1)
