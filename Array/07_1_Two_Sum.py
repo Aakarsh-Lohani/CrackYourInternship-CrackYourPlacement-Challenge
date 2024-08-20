9cde9f6adc5eb64852fc901caabc7f15387a604b
AAKARSH LOHANI <aakarshlohani01@gmail.com>
2024-08-20 23:46:52 +0530

    update solution

--- Array/07_1_Two_Sum.py
+++ Array/07_1_Two_Sum.py
@@ -11,3 +11,21 @@ class Solution:
             if target-nums[i] in d:
                 return (d[target-nums[i]],i)
             d[nums[i]]=i
+
+
+
+# For Sorted array
+# Two Pointer Approach
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+def two_sum_sorted(arr,target):
+    l=0
+    r=len(arr)-1
+    while l<r:
+        if arr[l]+arr[r]==target:
+            return (l,r)
+        elif arr[l]+arr[r]>target:
+            r-=1
+        else:
+            l+=1
+    return -1
